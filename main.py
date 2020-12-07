ceb1544a40f64c92fdc87895d942dabd52b9e9ef
Alex Coder <49398367+a1excoder@users.noreply.github.com>
2020-12-07 16:59:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,47 +2,52 @@ import requests
 import telebot
 
 url = 'http://api.openweathermap.org/data/2.5/weather'
-api_weather = ''
-api_telegram = ''
+api_weather = '19129b5ee4b41ccf1d9f5599d6b53b5f'
+api_telegram = '1214839484:AAFarMu-2QnEGU0gENOCvaXJpbzUDKxBkJ4'
 
 bot = telebot.TeleBot(api_telegram)
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-	sti = open('welcome.png', 'rb')
-	bot.send_sticker(message.chat.id, sti)
 	bot.send_message(message.chat.id, 'Добро пожаловать, ' + str(message.from_user.username) + ',' + '\n' +
-	 'Напиши мне название твоего города а я тебе информацию о погоде в твоем городе!')
+	 'чтоб узнать погоду напишите команду /weather <имя города>')
 
 
-@bot.message_handler(content_types=['text'])
-def weather_send(message):
-	s_city = message.text
+@bot.message_handler(commands=['help'])
+def welcome(message):
+	bot.send_message(message.chat.id, '/start запуск бота\n/help команды бота\n/weather <имя города>')
+
+
+@bot.message_handler(commands=['weather'])
+def test(message):
+	city_name = message.text[9:]
+	
 	try:
-		params = {'APPID': api_weather, 'q': s_city, 'units': 'metric'}
+		params = {'APPID': api_weather, 'q': city_name, 'units': 'metric', 'lang': 'ru'}
 		result = requests.get(url, params=params)
 		weather = result.json()
+
+
+		if weather["main"]['temp'] < 10:
+			status = "Сейчас холодно!"
+		elif weather["main"]['temp'] < 20:
+			status = "Сейчас прохладно!"
+		elif weather["main"]['temp'] > 38:
+			status = "Сейчас жарко!"
+		else:
+			status = "Сейчас отличная температура!"
 
 		bot.send_message(message.chat.id, "В городе " + str(weather["name"]) + " температура " + str(float(weather["main"]['temp'])) + "\n" + 
 				"Максимальная температура " + str(float(weather['main']['temp_max'])) + "\n" + 
 				"Минимальная температура " + str(float(weather['main']['temp_min'])) + "\n" + 
 				"Скорость ветра " + str(float(weather['wind']['speed'])) + "\n" + 
 				"Давление " + str(float(weather['main']['pressure'])) + "\n" + 
-				"Влажность " + str(float(weather['main']['humidity'])) + "\n" + 
+				"Влажность " + str(int(weather['main']['humidity'])) + "%" + "\n" + 
 				"Видимость " + str(weather['visibility']) + "\n" + 
-				"Описание " + str(weather['weather'][0]["description"]) + "\n")
-
-		if weather["main"]['temp'] < 10:
-			bot.send_message(message.chat.id, "Сейчас холодно!")
-		elif weather["main"]['temp'] < 20:
-			bot.send_message(message.chat.id, "Сейчас прохладно!")
-		elif weather["main"]['temp'] > 38:
-			bot.send_message(message.chat.id, "Сейчас жарко!")
-		else:
-			bot.send_message(message.chat.id, "Сейчас отличная температура!")
+				"Описание " + str(weather['weather'][0]["description"]) + "\n\n" + status)
 
 	except:
-		bot.send_message(message.chat.id, "Город " + s_city + " не найден")
+		bot.send_message(message.chat.id, "Город " + city_name + " не найден")
 
 
 if __name__ == '__main__':
