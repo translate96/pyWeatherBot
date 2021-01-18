944a1683e046d741a745f33d9d11580b0afe02d2
Alex Coder <49398367+a1excoder@users.noreply.github.com>
2021-01-18 17:57:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def welcome(message):
 
 @bot.message_handler(commands=['help'])
 def welcome(message):
-	bot.send_message(message.chat.id, '/start запуск бота\n/help команды бота\n/weather <имя города>')
+	bot.send_message(message.chat.id, '/start запуск бота\n/help команды бота\n/чтоб узнать погоду напишите в чат название города')
 
 
 @bot.message_handler(content_types=['text'])
